331435d41577d8e21ee2e3c1cd5da507d812ace5
nagesha technology <asbconstruction19@gmail.com>
2024-03-15 23:59:51 +0530

    Gocd pipeline for frontend

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
 
-ansible-playbook -i $component-$env.angadicnc.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
+ansible-playbook -i $component-$dev.angadicnc.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
